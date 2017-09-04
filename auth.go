904ccc27af4abab90811f44fdddbed4a315b0c90
m4dfry <m4dfry@zoho.com>
2017-09-04 14:15:24 +0200

    Added cookie for write realname in header of Index

--- auth.go
+++ auth.go
@@ -23,6 +23,7 @@ func LoginHandler(res http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	if value, ok := configs.Users[req.Form["username"][0]]; (ok && value.Password == req.Form["password"][0]) {
 		SetSecureSessionLogged(res, req, "true")
+		SetCookieRealname(res, value.RealName)
 		res.Write([]byte("true"))
 	} else {
 		res.Write([]byte("false"))
@@ -39,4 +40,9 @@ func SetSecureSessionLogged(w http.ResponseWriter, r *http.Request, value string
 	session.Values["logged"] = value
 	// Save it before we write to the response/return from the handler.
 	session.Save(r, w)
+}
+
+func SetCookieRealname(w http.ResponseWriter, value string) {
+	cookie := http.Cookie{Name: "realname", Value: value, Path: "/"}
+	http.SetCookie(w, &cookie)
 }
